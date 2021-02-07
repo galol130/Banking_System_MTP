b2e06fc8a4765981577f0bea8745e59d8e5d8628
Galo García Pugliese <galo130@gmail.com>
2021-02-07 18:33:32 +0100

    First commit

---
+++ src/test/java/com/ironhack/MidTerm/MidTermApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ironhack.MidTerm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MidTermApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
